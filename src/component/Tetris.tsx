0dfb004e2b7b974ed2bedb0fb14a1366fb2f317c
Eyre Xi <109657355+Eyre-dutchess@users.noreply.github.com>
2025-07-20 10:13:43 +0800

    Update Tetris.tsx

--- src/component/Tetris.tsx
+++ src/component/Tetris.tsx
@@ -2,14 +2,18 @@ import {  useTetris } from "@/hooks/useTetris"
 import { MainBoard } from "./MainBoard"
 import { HighScore } from "./HighScore"
 import { UpcomingBlocks } from "./UpcomingBlocks"
-import { StarBg } from "./StarBg"
+// import { StarBg } from "./StarBg"
+import gamesong from "@/assets/tetris_theme.mp3";
+
+const gameSong = new Audio(gamesong)
+gameSong.volume = 0.5
 
 export const Tetris = () => {
     const {board, startGame, rows, isPlaying,   score, upcomingBlocks} = useTetris()
     return(
         <>
-        <div className={`h-screen relative z-0 ${!isPlaying && "opacity-50"}`}>
-            <StarBg/>
+        <div className={`h-screen bg-black relative z-0 ${!isPlaying && "opacity-50"}`}>
+{/*             <StarBg/> */}
             <h1 className="relative z-40">Tetris</h1>
             <div className="h-3/4 relative z-40 min-h-[650px] bg-white/25 py-5 w-[800px] px-3 flex flex-row gap-2">
                 <UpcomingBlocks nextBlocks={upcomingBlocks}/>
@@ -38,7 +42,9 @@ export const Tetris = () => {
                     {score ===0 && rows === 0 && ""}
                     {score > 0 && rows > 0 && `You scored ${score}! Try again` }
                 </p>
-                <button onClick={()=>startGame()} className="bg-white/5  cursor-pointer transition flex items-center justify-center  
+                <button onClick={()=>{startGame()
+                                      gameSong.play()   }} 
+                    className="bg-white/5  cursor-pointer transition flex items-center justify-center  
                     w-1/3  py-[1em] rounded-full text-white  hover:bg-white/50 hover:shadow-lg border border-white/50">
                     Start Game
                 </button>
